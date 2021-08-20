cb9e5ae243513a82b60f3a11a215ca2946a04d7c
IcyLilith <69317946+IcyLilith@users.noreply.github.com>
2021-08-20 09:01:40 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Practical
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Please proved your ICAS number: ");
+            int icas = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please provide your course name: ");
+            string subject = Console.ReadLine();
+
+            Console.WriteLine("Student " + icas + " is in the " + subject + "course");
+        }
+    }
+}
