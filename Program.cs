65b1418794b3559059e48409057e05c511e0e813
Julian <69317955+Fishpuke@users.noreply.github.com>
2021-08-20 10:41:09 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -9,7 +9,7 @@ namespace Practical
             Console.WriteLine("Hello World!");
             Console.WriteLine("Please proved your ICAS number: ");
             int icas = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Please provide your course name: ");
+            Console.WriteLine("Please provide your course name: small change");
             string subject = Console.ReadLine();
 
             Console.WriteLine("Student " + icas + " is in the " + subject + " course.");
